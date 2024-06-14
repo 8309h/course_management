9f416623ee87c7e2f37c0ea984e38db4ea88d0ce
8309h <harshalwagh201718@gmail.com>
2024-06-14 19:51:30 +0530

    course_management

--- course-management/index.js
+++ course-management/index.js
@@ -3,8 +3,7 @@ const bodyParser = require('body-parser');
 const userRoutes = require('./routes/userRoutes');
 const courseRoutes = require('./routes/courseRoute');
 const progressRoutes = require('./routes/progressRoute');
-const { sequelize } = require('./config/dbConfig');
-const {connectionToDB} =  require('./config/dbConfig')
+const { sequelize, connectionToDB } = require('./config/dbConfig');
 require('dotenv').config();
 
 const app = express();
@@ -12,32 +11,25 @@ const app = express();
 app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
+  res.send("Welcome to course management");
+});
 
-    try{
-        res.send("welcome")
-    }catch(e){
-        console.log(e.message)
-    }
-  });
-app.use('/user', userRoutes);
+app.use('/api/v1/users', userRoutes); // Changed '/user' to '/api/v1/users' to maintain consistency with versioning
 app.use('/api/v1/courses', courseRoutes);
-app.use('/api/v1', progressRoutes);
-
+app.use('/api/v1/progress', progressRoutes);
 
 // Function to initialize server
 async function startServer() {
-    await connectionToDB(); 
-  
-    // Sync all models
-    await sequelize.sync();
-  
-    // Start the server
-    app.listen(process.env.ORT, () => {
-      console.log(`Server is running on http://localhost:${process.env.PORT}`);
-    });
-  }
-  
+  await connectionToDB(); 
+
+  // Sync all models
+  await sequelize.sync();
+
   // Start the server
-  startServer();
+  app.listen(process.env.PORT, () => {
+    console.log(`Server is running on http://localhost:${process.env.PORT}`);
+  });
+}
 
-
+// Start the server
+startServer();
